9ba52aeebf865943f691b4ee1dcd6a82256a7cf4
GDAWAR8648 <dawar.gagan@gmail.com>
2026-06-05 18:14:49 +0530

    bug fixed

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'xpenseai-v2';
+const CACHE = 'xpenseai-v3';
 const CORE = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
